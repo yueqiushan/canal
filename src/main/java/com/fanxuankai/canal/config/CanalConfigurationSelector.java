ed267ed59ad6392bf62adf4fe3334368c1cbaad5
fanxuankai <fan_xuankai@126.com>
2020-04-04 11:01:01 +0800

    unimport

--- src/main/java/com/fanxuankai/canal/config/CanalConfigurationSelector.java
+++ src/main/java/com/fanxuankai/canal/config/CanalConfigurationSelector.java
@@ -2,7 +2,6 @@ package com.fanxuankai.canal.config;
 
 import com.fanxuankai.canal.annotation.CanalEntityMetadataCache;
 import com.fanxuankai.canal.annotation.EnableCanalAttributes;
-import com.fanxuankai.canal.util.App;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
@@ -16,8 +15,10 @@ public class CanalConfigurationSelector implements ImportSelector {
         EnableCanalAttributes.from(importingClassMetadata);
         CanalEntityMetadataCache.load();
         // 使用 EnableCanal 注解时, 导入所需的 Bean
-        return new String[]{App.class.getName(), CanalConfig.class.getName(),
-                CanalConfigurationImporter.class.getName(),
-                ConsumerBeanRegister.class.getName()};
+        return new String[]{
+                CanalConfig.class.getName(),
+                ConsumerBeanRegister.class.getName(),
+                CanalConfigurationImporter.class.getName()
+        };
     }
 }
